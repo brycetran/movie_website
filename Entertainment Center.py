3196cfc85ff011e216e4b84d6c40d728404c9f68
Binh <binhntrancs@gmail.com>
2018-06-26 20:40:36 -0700

    update code style and add comments

--- Entertainment Center.py
+++ Entertainment Center.py
@@ -1,15 +1,18 @@
 import media
 import fresh_tomatoes
 
-ex_machina = media.Movie_id(264660)
-her = media.Movie_id(152601)
-the_imitation_game = media.Movie_id(205596)
+movie_display = 9
+
+ex_machina = media.MovieID(264660)
+her = media.MovieID(152601)
+the_imitation_game = media.MovieID(205596)
 # the_social_network = media.Movie_id(37799)
 # steve_jobs = media.Movie_id(321697)
 movies_list = [the_imitation_game, her, ex_machina]
 
-for i in range(9):
-   movies_list.append(media.Movie(i))
+# Create movie objects equal to movie_display and add to list
+for i in range(movie_display):
+    movies_list.append(media.Movie(i))
 
 fresh_tomatoes.open_movies_page(movies_list)
 
