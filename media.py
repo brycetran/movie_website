ea8e6e895d73ab5f2aa480fc5eb7e69478a3f23c
Binh <binhntrancs@gmail.com>
2018-06-26 20:41:21 -0700

    update code style and add comments

--- media.py
+++ media.py
@@ -3,27 +3,34 @@ tmdb.API_KEY = 'c8385eb8708b5c75026cee112688f1a9'
 image_base_url = 'https://image.tmdb.org/t/p/w500/'
 youtube_base_url = 'https://www.youtube.com/watch?v='
 
-class Movie_id:
+
+class MovieID:
     def __init__(self, movie_id):
+        # Access the Movie Database and return info about movie_id
         movie = tmdb.Movies(movie_id)
         movie.info()
-        self.title = str(movie.title) + "\n" + str(int(movie.vote_average * 10)) +"%"
+        self.title = movie.title + "\n" + str(int(movie.vote_average * 10)) \
+                     + "%"
         self.storyline = movie.overview
         movie.videos()
         self.poster_image_url = image_base_url + movie.poster_path
-        self.trailer_youtube_url = youtube_base_url + movie.results[0]['key']
+        key = movie.results[0]['key']
+        self.trailer_youtube_url = youtube_base_url + key
+
 
 class Movie:
-    def __init__(self, result_element):
+    def __init__(self, index):
+        # Acess the Movie Database and retrieve movie_id of a current movie base on index
         movie = tmdb.Movies()
         movie.now_playing()
-        movie_id = movie.results[result_element]['id']
-        # same code as above object
+        movie_id = movie.results[index]['id']
+        # Return information
         movie = tmdb.Movies(movie_id)
         movie.info()
-        self.title = str(movie.title) + "\n" + str(int(movie.vote_average * 10)) + "%"
+        self.title = movie.title + "\n" + str(int(movie.vote_average * 10)) \
+                     + "%"
         self.storyline = movie.overview
-        self.poster_image_url = image_base_url + movie.poster_path
         movie.videos()
+        self.poster_image_url = image_base_url + movie.poster_path
         key = movie.results[0]['key']
         self.trailer_youtube_url = youtube_base_url + key
